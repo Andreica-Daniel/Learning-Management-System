1fd54d3690cb7bec64fb419c85f07950292397bb
Andreica Daniel <dani.andreica28@gmail.com>
2021-07-28 11:33:48 +0300

    Added email-school_id index

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
   belongs_to :school, foreign_key: :school_id
 
   accepts_nested_attributes_for :school, reject_if: ->(attributes){ attributes['name'].blank? || attributes['subdomain'].blank? }, allow_destroy: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_22_085443) do
+ActiveRecord::Schema.define(version: 2021_07_28_082205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2021_07_22_085443) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "school_id"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email", "school_id"], name: "index_users_on_email_and_school_id", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["school_id"], name: "index_users_on_school_id"
   end
